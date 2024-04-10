1c78af1fdddfb634f9281aac8bbac429b101a013
The-Tree-Knight <159048768+The-Tree-Knight@users.noreply.github.com>
2024-04-10 11:13:20 -0700

    Update Rock, Paper, Scissors.py
    Replace bloated code with getValidInt

--- Rock, Paper, Scissors.py
+++ Rock, Paper, Scissors.py
@@ -4,16 +4,16 @@
 import random
 
 def main():
-    answer= remove_non_letters(list(input('Do you want to play Rock, Paper, Scissors? (yes or no): ').lower()))
+    answer= removeNonLetters(list(input('Do you want to play Rock, Paper, Scissors? (yes or no): ').lower()))
     while True:
         if answer== ['y', 'e', 's'] or answer== ['y']:
             break
         elif answer== ['n', 'o'] or answer== ['n']:
-            answer= remove_non_letters(list(input('Please try again\n').lower()))
+            answer= removeNonLetters(list(input('Please try again\n').lower()))
         else:
-            answer= remove_non_letters(list(input('Huh what? can you say it again\n').lower()))
+            answer= removeNonLetters(list(input('Huh what? can you say it again\n').lower()))
     while True:
-        numberToPlay= getNumberOfTimes('How many wins do we play to?\n')
+        numberToPlay= getValidInt('How many wins do we play to?\n')
         wins= 0
         losses= 0
         ties= 0
@@ -34,7 +34,7 @@ def main():
             print('You\'ve won the whole thing!')
         else:
             print('YAY! I\'VE WON')
-        playAgain= remove_non_letters(list(input('Do you want to play Rock, Paper, Scissors again? (yes or no): ').lower()))
+        playAgain= removeNonLetters(list(input('Do you want to play Rock, Paper, Scissors again? (yes or no): ').lower()))
         while True:
             if playAgain== ['y', 'e', 's'] or playAgain== ['y']:
                 again= 'Yes'
@@ -43,42 +43,29 @@ def main():
                 again= 'No'
                 break
             else:
-                playAgain= remove_non_letters(list(input('Huh what? can you say it again ').lower()))
+                playAgain= removeNonLetters(list(input('Huh what? can you say it again?\n').lower()))
         if again== 'No':
             break
     print('Thank you for playing')
     
-def getNumberOfTimes(msg):
-    """Asks the user how many times they would like to do something"""
-    total= 0
+def getValidInt(msg):
+    '''Gets a Integer above zero'''
     while True:
-        userInput= list(input(msg))
-        while ' ' in userInput:
-            userInput.remove(' ')
-        if userInput[0] == '-':
-            print('I can\'t play negative times')
-            continue
-        numberToPlay= removeNonNumbers(userInput)
-        if numberToPlay== []:
-            print('Sorry, I only understand numbers')
-        else:
-            for number in range(len(numberToPlay)):
-                digit= int(numberToPlay.pop())*(10**(number))
-                total += digit
-            return total
-
-
-def removeNonNumbers(word):
-    """Removes non numbers from a list"""
-    word2= word.copy()
-    for item in word2:
-        if not '0' <= item <= '9':
-            word.remove(item)
-    return word
+        try:
+            validInt= int(input(msg))
+            if validInt < 0:
+                print('I can\'t play negative times\n')
+            elif validInt == 0:
+                print('You said you would play!\n')
+            else:
+                return validInt
+        except ValueError:
+            print('Sorry, I only hear numbers\n')
+    
 
 def getPlayerHandPosition(msg):
     """Takes a list of inputs and returns a string"""
-    userInput= remove_non_letters(list(input(msg).lower()))
+    userInput= removeNonLetters(list(input(msg).lower()))
     if userInput== ['r', 'o', 'c', 'k'] or userInput== ['r']:
         position= 'Rock'
         return position
@@ -91,7 +78,7 @@ def getPlayerHandPosition(msg):
     else:
         return 'Invalid'
 
-def remove_non_letters(word):
+def removeNonLetters(word):
     """Removes non letters from a list"""
     word2= word.copy()
     for item in word2:
@@ -102,7 +89,7 @@ def remove_non_letters(word):
 
 def getAiHandPosition(allRockChance):
     '''Code for how the AI acts'''
-    if allRockChance== 1:
+    if allRockChance== 100:
         return 'Rock'
     number= random.randint(1, 3)
     if number== 1:
@@ -151,7 +138,9 @@ def whoWins(playerPosition, aiPosition):
         print('Ohhh, a Tie')
         return 'Tie'
     else:
+        print(f'{playerPosition} vs {aiPosition}')
         print('That isn\'t a valid move')
     
 
 main()
+        
